a3a952035cd02bd86f1ccaf5d905942580198733
nicolefox <fox_nicole@hotmail.com>
2021-06-29 17:17:16 -0300

    1.0.27 else node

--- cli/src/main/java/cli/CLI.java
+++ cli/src/main/java/cli/CLI.java
@@ -15,16 +15,28 @@ import parser.*;
 import parser.nodes.ASTNode;
 
 public class CLI {
-  public static void main(String[] args, String version, Consumer<String> emitter) {
-    DefaultLexer lexer = new DefaultLexer(version);
+//  public static void main(String[] args, String version, Consumer<String> emitter) {
+//    DefaultLexer lexer = new DefaultLexer(version);
+//    Parser parser = new Parser();
+//    List<String> auxPrint = new ArrayList<>();
+//    Interpreter interpreter = new Interpreter(new Terminal(), auxPrint);
+//    FileReader fileReader = new DefaultFileReader();
+//    List<Token> tokenStream = lexer.lex(fileReader.readFile(args[0]));
+//    Provider input = new TokenProvider(tokenStream);
+//    ASTNode ast = parser.parse(input);
+//    interpreter.start(ast);
+//    interpreter.getEmitter().forEach(emitter::accept);
+//  }
+  public static void main(String[] args) {
+    DefaultLexer lexer = new DefaultLexer("1.1");
+    String text = "if(true) { println(\"hola\"); } else { println(123); }";
     Parser parser = new Parser();
     List<String> auxPrint = new ArrayList<>();
     Interpreter interpreter = new Interpreter(new Terminal(), auxPrint);
     FileReader fileReader = new DefaultFileReader();
-    List<Token> tokenStream = lexer.lex(fileReader.readFile(args[0]));
+    List<Token> tokenStream = lexer.lex(text);
     Provider input = new TokenProvider(tokenStream);
     ASTNode ast = parser.parse(input);
     interpreter.start(ast);
-    interpreter.getEmitter().forEach(emitter::accept);
   }
 }
