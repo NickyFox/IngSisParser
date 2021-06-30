0eea951278ca1cc580bb9e0bca2c2925f02b97ce
nicolefox <fox_nicole@hotmail.com>
2021-06-30 10:30:20 -0300

    tests

--- parser/src/test/java/lexer/ParserTest.java
+++ parser/src/test/java/lexer/ParserTest.java
@@ -1,68 +1,171 @@
 package lexer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import common.provider.Provider;
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Scanner;
+import lexer.provider.TokenProvider;
+import lexer.tokens.Token;
+import org.junit.Assert;
+import org.junit.Test;
+import parser.Parser;
 import parser.nodes.*;
 
-// public class ParserTest {
+public class ParserTest {
+
+    Lexer lexer = new DefaultLexer("1.0");
+    Parser parser = new Parser();
+    ObjectMapper objectParser = new ObjectMapper();
+
+    @Test
+    public void shouldParseExpectedASTFromStringDeclaration() {
+
+        // Given input
+        String declaration = "let str: string = \"someString\";";
+        List<Token> tokens = lexer.lex(declaration);
+        Provider input = new TokenProvider(tokens);
+        ASTNode node = parser.parse(input);
+
+        try {
+            boolean result =
+                    checkNodeMatchesFileResult(
+                            node, "testing-resources/expectedASTFromStringDeclaration.json");
+            Assert.assertTrue(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertEquals(false, true);
+        }
+    }
+
+    @Test
+    public void shouldParseExpectedASTFromBooleanDeclaration() {
+        // Given input
+        String declaration = "let bo: boolean = true;";
+        List<Token> tokens = lexer.lex(declaration);
+        Provider input = new TokenProvider(tokens);
+        ASTNode node = parser.parse(input);
+
+        try {
+            boolean result =
+                    checkNodeMatchesFileResult(node, "testing-resources/expectedBooleanDeclaration.json");
+            Assert.assertTrue(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertEquals(false, true);
+        }
+    }
+
+    @Test
+    public void shouldParseExpectedASTFromCombinedDeclaration() {
+
+        // Given input
+        String declaration = "let str: string = \"someString\";\n" + "let bo: boolean = true;";
+        List<Token> tokens = lexer.lex(declaration);
+        Provider input = new TokenProvider(tokens);
+        ASTNode node = parser.parse(input);
+
+        try {
+            boolean result =
+                    checkNodeMatchesFileResult(
+                            node, "testing-resources/expectedASTFromCombinedDeclaration.json");
+            Assert.assertTrue(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertEquals(false, true);
+        }
+    }
+
+    @Test
+    public void shouldCreateABooleanNode() {
+
+        ExpressionNode exprNode = new BooleanNode(true);
+        exprNode.setLeft(new BooleanNode(true));
+        ProgramNode node = new ProgramNode();
+        node.add(new DeclarationNode(new IdentifierNode("bo"), "boolean", exprNode, false));
+
+        try {
+            boolean result =
+                    checkNodeMatchesFileResult(node, "testing-resources/expectedBooleanNodeCreation.json");
+            Assert.assertTrue(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertEquals(false, true);
+        }
+    }
+
+    @Test
+    public void shouldCreateAStringNode() {
+        ExpressionNode exprNode = new StringNode("someString");
+        exprNode.setLeft(new StringNode("someString"));
+        ProgramNode node = new ProgramNode();
+        node.add(new DeclarationNode(new IdentifierNode("str"), "string", exprNode, false));
+        try {
+            boolean result =
+                    checkNodeMatchesFileResult(node, "testing-resources/expectedStringNodeCreation.json");
+            Assert.assertTrue(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertEquals(false, true);
+        }
+    }
+
+    @Test
+    public void shouldCreateANumberNode() {
+        ExpressionNode exprNode = new IntegerNode(1);
+        exprNode.setLeft(new IntegerNode(1));
+        ProgramNode node = new ProgramNode();
+        node.add(new DeclarationNode(new IdentifierNode("integerNumber"), "number", exprNode, false));
+
+        try {
+            boolean result =
+                    checkNodeMatchesFileResult(node, "testing-resources/expectedNumberNodeCreation.json");
+            Assert.assertTrue(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertEquals(false, true);
+        }
+    }
+
+//    @Test
+//    public void shouldCreateAPrintNode() {
+//        ExpressionNode exprNode = new PrintNode(1);
+//        exprNode.setLeft(new IntegerNode(1));
+//        ProgramNode node = new ProgramNode();
+//        node.add(new DeclarationNode(new IdentifierNode("integerNumber"), "number", exprNode, false));
 //
-//  Lexer lexer = new DefaultLexer("1.0");
-//  Parser parser = new Parser();
+//        try {
+//            objectParser.writeValue(new File("testing-resources/expectedPrintNodeCreation.json"), node);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 //
-//  @Test
-//  public void shouldParseExpectedASTFromStringDeclaration() {
-//
-//    // Given input
-//    String declaration = "let str: string = \"some string\";";
-//    List<Token> tokens = lexer.lex(declaration);
-//    Provider input = new TokenProvider(tokens);
-//    ASTNode node = parser.parse(input);
-//
-//    // Generating expected result for input
-//    ExpressionNode exprNode = new StringNode("some string");
-//    exprNode.setLeft(new StringNode("some string"));
-//    ProgramNode expected = new ProgramNode();
-//    expected.add(new DeclarationNode(new IdentifierNode("str"), "string", exprNode, false));
-//
-//    Assert.assertEquals(node, expected);
-//  }
-//
-//  @Test
-//  public void shouldParseExpectedASTFromBooleanDeclaration() {
-//    // Given input
-//    String declaration = "let bo: boolean = true;";
-//    List<Token> tokens = lexer.lex(declaration);
-//    Provider input = new TokenProvider(tokens);
-//    ASTNode node = parser.parse(input);
-//
-//    ExpressionNode exprNode = new BooleanNode(true);
-//    exprNode.setLeft(new BooleanNode(true));
-//
-//    // Generating expected result for input
-//    ProgramNode expected = new ProgramNode();
-//    expected.add(new DeclarationNode(new IdentifierNode("bo"), "boolean", exprNode, false));
-//
-//    Assert.assertEquals(node, expected);
-//  }
-//
-//  @Test
-//  public void shouldParseExpectedASTFromCombinedDeclaration() {
-//
-//    // Given input
-//    String declaration = "let str: string = \"some string\";\nlet bo: boolean = true;";
-//    List<Token> tokens = lexer.lex(declaration);
-//    Provider input = new TokenProvider(tokens);
-//    ASTNode node = parser.parse(input);
-//
-//    // Generating expected result for input
-//    ProgramNode expected = new ProgramNode();
-//    ExpressionNode strExprNode = new StringNode("some string");
-//    strExprNode.setLeft(new StringNode("some string"));
-//
-//    expected.add(new DeclarationNode(new IdentifierNode("str"), "string", strExprNode, false));
-//    ExpressionNode exprNode = new BooleanNode(true);
-//    exprNode.setLeft(new BooleanNode(true));
-//
-//    expected.add(new DeclarationNode(new IdentifierNode("bo"), "boolean", exprNode, false));
-//
-//    Assert.assertEquals(node, expected);
-//  }
-// }
+//        try {
+//            boolean result =
+//                    checkNodeMatchesFileResult(node, "testing-resources/expectedPrintNodeCreation.json");
+//            Assert.assertTrue(result);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            Assert.assertEquals(false, true);
+//        }
+//    }
+
+    /** Returns true if expected matches current */
+    private boolean checkNodeMatchesFileResult(ASTNode node, String url) {
+        try {
+            String actualResult = objectParser.writeValueAsString(node);
+            FileReader fr = new FileReader(url);
+            Scanner scanner = new Scanner(fr);
+            String expectedResult = "";
+            while (scanner.hasNext()) {
+                expectedResult = expectedResult + scanner.next();
+            }
+            System.out.println(actualResult);
+            System.out.println(expectedResult);
+            return actualResult.equals(expectedResult);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
